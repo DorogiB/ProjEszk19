8d025c4380fa950f05264b1d0d77bb2ebb489c49
Gabor Kalapis <kgabor0420@gmail.com>
2019-04-03 17:32:44 +0200

    added test for repositories, h2 config is still missing

---
+++ BACKEND/src/test/java/hu/elte/alkfejl/alkfejl18/RepositoryTest.java
@@ -0,0 +1,56 @@
+package hu.elte.alkfejl.alkfejl18;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import hu.elte.alkfejl.alkfejl18.entities.Skill;
+import hu.elte.alkfejl.alkfejl18.entities.User;
+import hu.elte.alkfejl.alkfejl18.repositories.SkillRepository;
+import hu.elte.alkfejl.alkfejl18.repositories.UserRepository;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class RepositoryTest {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private SkillRepository skillRepository;
+	
+	@Test
+	public void whenFindByUserName_thenReturnUser() {
+	    // given
+		String userName = "alex";
+	    User user = new User();
+	    user.setUsername(userName);
+	
+	    // when
+	    userRepository.save(user);
+	 	 
+	    // then
+	    User savedUser = userRepository.findByUsername(userName).get();
+	    assertEquals(savedUser.getName(), user.getName());
+	}
+	
+	@Test
+	public void whenFindByName_thenReturnSkill() {
+	    // given
+		String name = "very_fancy_skill";
+	    Skill skill = new Skill();
+	    skill.setName(name);
+	
+	    // when
+	    skillRepository.save(skill);
+	 	 
+	    // then
+	    Skill savedSkill = skillRepository.findByName(name).get();
+	    assertEquals(savedSkill.getName(), savedSkill.getName());
+	}
+	
+}
