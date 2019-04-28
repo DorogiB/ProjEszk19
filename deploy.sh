fa1c0d1fbbfb15a024d570cad67086e587ef2f48
AKCKD0 <feauaai@inf.elte.hu>
2019-04-28 19:01:12 +0200

    Compodoc deployment to GitHub Pages

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -p -e "require('./package.json').repository.url")}
+
+cd documentation && \
+
+rm -R .git
+git init && \
+
+git config user.name "DorogiB" && \
+git config user.email "dbenjamin@inf.elte.hu" && \
+
+git add . && \
+git commit -m "Deploy to GitHub Pages" && \
+
+git push --force "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:gh-pages > /dev/null 2>&1
