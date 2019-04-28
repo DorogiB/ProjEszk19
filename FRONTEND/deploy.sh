917c31de2f14eff542d0d005a19b1330356da253
AKCKD0 <feauaai@inf.elte.hu>
2019-04-28 19:15:05 +0200

    deploy.sh updated

--- FRONTEND/deploy.sh
+++ FRONTEND/deploy.sh
@@ -10,7 +10,7 @@ git init && \
 git config user.name "DorogiB" && \
 git config user.email "dbenjamin@inf.elte.hu" && \
 
-git add . && \
+git add .
 #git commit -m "Deploy to GitHub Pages" && \
 #
 #git push --force "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:gh-pages > /dev/null 2>&1
