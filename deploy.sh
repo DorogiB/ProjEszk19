0ec289d3488327ec05b9f9aacf34ea7224346f2d
AKCKD0 <feauaai@inf.elte.hu>
2019-05-20 01:30:45 +0200

    Some update

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# cp module-backend/target/module-backend*.war deployment/ && \
+cp module-backend/target/module-backend*.war deployment/ && \
 
-# git config user.name "DorogiB" && \
-# git config user.email "dbenjamin@inf.elte.hu" && \
+git config user.name "DorogiB" && \
+git config user.email "dbenjamin@inf.elte.hu" && \
 
-# git add deployment/* && \
-# git commit -m "Deploying application" && \
+git add deployment/* && \
+git commit -m "Deploying application" && \
 
-# git push --force "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:master
+git push --force "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:master
