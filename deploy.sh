88affb79acc77412c7941742a13b38aaaa20a956
AKCKD0 <feauaai@inf.elte.hu>
2019-05-20 01:38:41 +0200

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -p -e "require('./package.json').repository.url")}
+
 cp module-backend/target/module-backend*.war deployment/ && \
 
 git config user.name "DorogiB" && \
 git config user.email "dbenjamin@inf.elte.hu" && \
 
-git add deployment/* && \
+git add . && \
 git commit -m "Deploying application" && \
 
 git push --force "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:master
