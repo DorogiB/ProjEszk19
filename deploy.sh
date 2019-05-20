e8c69a72cec4908ed302afbd082f781459b7fced
AKCKD0 <feauaai@inf.elte.hu>
2019-05-20 03:47:10 +0200

    Some updates

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,8 @@ cp module-backend/target/module-backend*.war deployment && \
 
 git add deployment/* && \
 git commit --message "Deploying application" && \
+git tag v$GIT_TAG_VERSION -a -m "Tagging version v$GIT_TAG_VERSION" && \
 
-git push --quiet --set-upstream "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:master
+# git push --quiet --set-upstream "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:master
+git push origin $TRAVIS_BRANCH
+git push origin $TRAVIS_BRANCH --tags
